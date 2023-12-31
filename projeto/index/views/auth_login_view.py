73fb1cd56e6bbad7f6fd20b9c8c5989426f8ad96
Joel <joelvieirasouza@gmail.com>
2023-12-31 20:10:53 -0300

    Update auth_login_view.py

--- projeto/index/views/auth_login_view.py
+++ projeto/index/views/auth_login_view.py
@@ -1,29 +1,21 @@
-from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-from django.http import JsonResponse
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
-from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
-from index.serializers import UserSerializer
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
+
 
 def auth_login(request):
-    return render(request, 'auth-login.html')
-
-
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def login_view(request):
-    if request.method == 'POST':
-        username = request.data.get('username')
-        password = request.data.get('password')
-        user = authenticate(request, username=username, password=password)
-
-        if user is not None:
-            login(request, user)
-            token, created = Token.objects.get_or_create(user=user)
-            return JsonResponse({'token': token.key, 'user': UserSerializer(user).data})
+    if request.method == 'GET':
+        return render(request, 'auth-login.html', {
+            'form' : AuthenticationForm
+        })
+        
+    else:
+        user = authenticate(request, email=request.POST['email'], password=request.POST['password'])
+        if user is None:
+            return render(request, '/auth_login', {
+            'form' : AuthenticationForm,
+            "error" : "Usuário ou senha inválido."                
+            })
         else:
-            return JsonResponse({'error': 'Invalid credentials'}, status=401)
-
-    return JsonResponse({'error': 'Method not allowed'}, status=405)
+            login(request, user)
+            return redirect('/index')
