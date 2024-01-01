620721dcb670a5b9e81f2d390f7fb04a13415fe0
Joel <joelvieirasouza@gmail.com>
2024-01-01 15:29:10 -0300

    Update auth_register_view.py

--- projeto/index/views/auth_register_view.py
+++ projeto/index/views/auth_register_view.py
@@ -1,10 +1,9 @@
-from django.shortcuts import redirect, render, get_object_or_404
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 
-def auth_register(request):
-    
+def auth_register(request):    
     if request.method == 'GET':
         return render(request, 'auth-register.html', {
             'form' : UserCreationForm
@@ -17,14 +16,14 @@ def auth_register(request):
                 user = User.objects.create_user(username=request.POST['fullname'], email=request.POST['email'], password=request.POST['password'])
                 user.save()
                 login(request, user)
-                return redirect('/auth_login')
+                return redirect('auth-register.html')
             except:
                 return render(request, 'auth-register.html', {
                 'form' : UserCreationForm,
                 "error" : "Usuário ja cadastrado."                
                 })
                 
-    return render(request, '/auth_register', {
+    return render(request, 'auth-register.html', {
         'form' : UserCreationForm,
         "error" : "Senhas diferentes."
     })
