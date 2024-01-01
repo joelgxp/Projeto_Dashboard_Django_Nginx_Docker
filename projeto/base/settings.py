1e8d8980ad81e64b826e4cca6881f0ddc7bd90f3
Joel <joelvieirasouza@gmail.com>
2024-01-01 15:29:02 -0300

    Update settings.py

--- projeto/base/settings.py
+++ projeto/base/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = ['http://*', 'https://*']
+
+
 TEMPLATES_DIR = BASE_DIR / 'templates'
 
 
@@ -119,7 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_ROOT = '/static/'
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
