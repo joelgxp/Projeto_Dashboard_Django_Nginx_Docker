9fa725163664768befddca1d30c7082fe239fa5b
Joel <joelvieirasouza@gmail.com>
2024-01-05 17:43:58 -0300

    Update lider_view.py

--- projeto/index/views/lider_view.py
+++ projeto/index/views/lider_view.py
@@ -5,21 +5,16 @@ def lider(request):
     lideres = LiderModel.objects.all()
     return render(request, 'lider.html', {'lideres': lideres})
 
-def criar_editar_lider(request, lider_id=None):
-    # Verifique se o líder existe (para edição) ou crie um novo (para criação)
-    if lider_id:
-        lider = LiderModel.objects.get(pk=lider_id)
-    else:
-        lider = LiderModel()
-
-    if request.method == 'POST':
-        # Crie um formulário com os dados do POST e os dados existentes do líder
-        form = LiderForm(request.POST, instance=lider)
+def criar_lider(request):
+    
+    if request.method == 'POST': 
+        form = LiderForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('/lider')
-    else:
-        # Crie um formulário vazio (ou preenchido com os dados existentes do líder)
-        form = LiderForm(instance=lider)
+            return redirect('/lider')             
 
-    return render(request, 'formulario.html', {'form': form})
+    else:
+        return render(request, 'lider.html', {
+            'form': form,
+        })
+            
