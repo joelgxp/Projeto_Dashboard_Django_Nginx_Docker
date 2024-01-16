5b700f3970f28e521bc1afdbf9a5e32ce8046c65
Joel <joelvieirasouza@gmail.com>
2024-01-16 01:23:23 -0300

    Update usuarios_view.py

--- projeto/index/views/usuarios_view.py
+++ projeto/index/views/usuarios_view.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from ..models.usuario_model import Usuario
+from ..models import Usuario
 from ..serializers.usuarios_serializer import UsuariosSerializer
 from rest_framework import status as status_code
 
