6e87cf602d85ca5fd3c5ca061de77d34383bc006
Joel <joelvieirasouza@gmail.com>
2024-01-16 01:23:28 -0300

    Update usuarios_serializer.py

--- projeto/index/serializers/usuarios_serializer.py
+++ projeto/index/serializers/usuarios_serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models.usuario_model import Usuario
+from ..models import Usuario
 
 class UsuariosSerializer(serializers.ModelSerializer):
     class Meta:
