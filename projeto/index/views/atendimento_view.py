55880ab717c7344afe22402f509d3981c0f40a24
Joel <joelvieirasouza@gmail.com>
2024-02-01 22:47:24 -0300

    Update atendimento_view.py

--- projeto/index/views/atendimento_view.py
+++ projeto/index/views/atendimento_view.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
 
+from index.models import PacienteModel, PacienteExameForm
+
 def atendimento(request):
-    return render(request, 'fila-atendimento.html')
+    if request.method == 'GET':
+        pacientes_lista = PacienteModel.objects.all()
+        if pacientes_lista.filter(atendido=True):
+            pacientes = PacienteModel.objects.filter(atendido=True)
+            return render(request, 'atendimento.html', {'pacientes_em_atendimento': pacientes})
+        else:
+            pacientes = PacienteModel.objects.filter(atendido=False)
+            return render(request, 'atendimento.html', {'pacientes_atendidos': pacientes})
+    elif request.method == 'POST':
+        
+        return render(request, 'atendimento.html')
+    
+def atendimento_preenche_exame(request, id):
+    
+    paciente = PacienteModel.objects.get(id=id)
+    paciente_id = paciente.id
+    
+    if request.method == 'POST':    
+        form = PacienteExameForm(request.POST)
+        if form.is_valid():
+            exame = form.save(commit=False)
+            exame.paciente = paciente_id
+            exame.save()
+        
+    else:
+        form = PacienteExameForm()
+        print('erro')
+    
+    return render(request, 'atendimento-exame.html', {'form': form, 'paciente': paciente})
