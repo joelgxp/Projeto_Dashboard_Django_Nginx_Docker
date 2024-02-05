1924c00614a70adc07a0a11e6179c3f1e21c4e4e
Joel <joelvieirasouza@gmail.com>
2024-02-05 13:12:48 -0300

    Update paciente_view.py

--- projeto/index/views/paciente_view.py
+++ projeto/index/views/paciente_view.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 
 from index.models import Paciente, PacienteForm
 
@@ -28,10 +29,18 @@ def paciente_cadastro(request):
             
     return render(request, 'paciente-cadastro.html', {'form': form})       
 
-def paciente_encaminha_atendimento(id):
+def paciente_encaminha_atend(id):
+    try:
+        # Verifica se o paciente existe no banco de dados
         paciente = Paciente.objects.get(id=id)
-        paciente.atendido = True
-        paciente.save()
+        print(paciente)
+    except ObjectDoesNotExist:
+        # Tratar o caso em que o paciente não existe
+        print(f"Paciente com ID {id} não encontrado.")
+        return
+
+    # Atualiza o estado do paciente para atendido
+    Paciente.objects.filter(id=id).update(atendido=True)
         
 @login_required
 def paciente_editar(request, id):    
@@ -47,5 +56,19 @@ def paciente_editar(request, id):
             print('erro')
     else:
         form = PacienteForm(instance=paciente)
+        
+def paciente_ficha_impressao(request, id):    
+    paciente = Paciente.objects.get(id=id)
+    
+    if request.method == 'POST':
+        form = PacienteForm(request.POST, instance=paciente)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('paciente'))
+        else:
             
-    return render(request, 'paciente-editar.html', {'form': form, 'paciente': paciente})
+            print('erro')
+    else:
+        form = PacienteForm(instance=paciente)
+            
+    return render(request, 'paciente-ficha-impressao.html', {'form': form, 'paciente': paciente})
