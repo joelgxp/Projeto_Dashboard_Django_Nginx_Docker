23361fbe5bd3081024b5aa74ff151e0524995662
Joel <joelvieirasouza@gmail.com>
2024-02-06 01:38:00 -0300

    Update settings.py
    USE_TZ = False

--- projeto/base/settings.py
+++ projeto/base/settings.py
@@ -101,13 +101,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'pt-BR'
 
 TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
