a1e5f192d47b67573345d9ac1af8a6bc1667c18f
Joel <joelvieirasouza@gmail.com>
2024-02-06 23:53:00 -0300

    Update admin.py

--- projeto/index/admin.py
+++ projeto/index/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
-from .models import Usuario
+from .models import Usuario, Paciente, PacienteExame
 
 admin.site.register(Usuario)
-
-
-
-# Register your models here.
+admin.site.register(Paciente)
+admin.site.register(PacienteExame)
